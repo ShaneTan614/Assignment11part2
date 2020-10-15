10d1fa4c5ce7485f33296816116485cf878e656e
ShaneTan614 <72852464+ShaneTan614@users.noreply.github.com>
2020-10-15 14:56:47 +0800

    Initial commit

---
+++ Assets/Scenes/scripts/playercontroller11.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller11 : MonoBehaviour
+{
+    float speed = 10.0f;
+    float maxLimit = 4.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        float jumpInput = Input.GetAxis("Jump");
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * HorizontalInput * speed);
+        transform.Translate(Vector3.up * Time.deltaTime * jumpInput * speed);
+
+        if(transform.position.z < -maxLimit)
+        {
+            transform.position(new Vector3(transform))
+        }
+    }
+}
